e4c470eae0ec69689cfc56ef6f3434ae718c8ebd
Jaime Pineda <jaime.e.pineda@gmail.com>
2024-12-10 11:51:18 +0100

    update to code -better handling of folders -common name definitions -work one line at a time

--- src/noema_combine/data_handler.py
+++ src/noema_combine/data_handler.py
@@ -57,7 +57,6 @@ for key_i in list_source_name:
     source_print += key_i + ", "
 
 ignorefiles = []
-# config['file_handling']['ignorefiles']
 for key, item in config.items("file_handling"):
     if key.startswith("ignorefiles"):
         ignorefiles.append(item)
@@ -132,14 +131,62 @@ def get_source_param(source_name: str) -> tuple[str, str, str, float, float, flo
     )
 
 
-def get_uvt_file(source_name: str, line_name: str, QN: str, Lid: str) -> str:
-    uvt_filename = f"{source_name}_{line_name}_{QN}_{Lid}.uvt"
+def get_uvt_file(source_name: str, line_name: str, QN: str, Lid: str, merge: bool = False) -> str:
+    """
+    Function to generate the output file name for NOEMA uvt file.
+    The format will be:
+    {dir_out}/{source_name}_{line_name}_{QN}_{Lid}.uvt
+    where {dir_out} depends on the merge parameter.
+
+    parameters:
+    -----------
+    source_name: str
+        Name of the source to reduce, e.g., "B5"
+    line_name: str
+        Molecule to reduce, e.g., "CO", "13CO", "N2H+"
+    QN: str
+        Quantum numbers of the line to reduce, e.g., "1-0" or "N=1-0,J=3/2-1/2,F=1/2-1/2"
+    Lid: str
+        Window unit for the NOEMA data, e.g., "L09", "l11", depending on the specification from the line catalogue.
+    merge: bool
+        If True, the file will be saved in the merge folder, otherwise in the uvt folder.
+    """
+    if merge == False:
+        dir_out = uvt_dir
+    else:
+        dir_out = uvt_dir_out
+    uvt_filename = os.path.join(
+        dir_out, Lid, f"{source_name}_{line_name}_{QN}_{Lid}.uvt")
     return uvt_filename
 
 
-def get_30m_file(source_out: str, line_name: str, QN: str) -> str:
-    outputfile = "{0}{1}_{2}_{3}.30m".format(
-        dir_30m, source_out, line_name, QN)
+def get_30m_file(source_name: str, line_name: str, QN: str, Lid: str, merge: bool = False) -> str:
+    """
+    Function to generate the output file name for the 30m data.
+    The format will be:
+    {dir_30}{source_out}_{line_name}_{QN}.30m
+    of
+    {dir_30}{source_out}_{line_name}_{QN}_{Lid}.30m
+    depending on the merge parameter.
+
+    parameters:
+    -----------
+    source_name: str
+        Name of the source to reduce, e.g., "B5"
+    line_name: str
+        Molecule to reduce, e.g., "CO", "13CO", "N2H+"
+    QN: str
+        Quantum numbers of the line to reduce, e.g., "1-0" or "N=1-0,J=3/2-1/2,F=1/2-1/2"
+    Lid: str
+        Window unit for the NOEMA data, e.g., "L09", "l11", depending on the specification from the line catalogue.
+    merge: bool
+        If True, the file will include the Lid information in the name.
+    """
+    if merge:
+        name_out = f"{source_name}_{line_name}_{QN}_{Lid}.30m"
+    else:
+        name_out = f"{source_name}_{line_name}_{QN}.30m"
+    outputfile = os.path.join(dir_30m, name_out)
     return outputfile
 
 
@@ -153,35 +200,46 @@ def line_prepare_merge(source_name: str, line_i: str, QN_i: str) -> None:
     parameters:
     -----------
     source_name: str
-        Name of the source to reduce
-    line: list
-        List of the line to reduce
-    QNs: list
-        List of the Quantum numbers of the line to reduce
+        Name of the source to reduce, e.g., "B5"
+    line: str
+        Molecule to reduce, e.g., "CO", "13CO", "N2H+"
+    QN: str
+        Quantum numbers of the line to reduce, e.g., "1-0" or "N=1-0,J=3/2-1/2,F=1/2-1/2"
     """
     _, _, source_out, _, _, _ = get_source_param(source_name)
 
     print(f"[INFO] Reducing line: {line_i} with QN: {QN_i}")
     index = get_line_param(line_i, QN_i)
-    print(source_out, line_name[index][0], QN[index][0])
-    inputfile = "{0}_{1}_{2}.30m".format(
-        source_out, line_name[index][0], QN[index][0])
+    QN_name_i = QN[index][0]
+    line_name_i = line_name[index][0]
+    print(source_out, line_name_i, QN_name_i)
+    # Already reduced 30m data
+    # data_30m = get_30m_file(source_out, line_name[index][0], QN[index][0])
+    # inputfile = "{0}_{1}_{2}.30m".format(
+    #     source_out, line_name[index][0], QN[index][0])
     freq_i = freq[index][0].astype(float) * 1e3
     Lid_i = Lid[index][0]
-    uvt_filename = get_uvt_file(source_out, line_i, QN[index][0], Lid_i)
+    file_uvt = get_uvt_file(source_out, line_name_i,
+                            QN_name_i, Lid_i, merge=False)
+    merge_uvt = get_uvt_file(source_out, line_name_i,
+                             QN_name_i, Lid_i, merge=True)
     # uvt_filename = f'{source_out}_{line_i}_{QN[index][0]}_{Lid_i}.uvt'
-    print(uvt_dir, Lid_i, uvt_filename)
-    uvt_file = uvt_dir + Lid_i + "/" + uvt_filename
-    outputfile = dir_30m + uvt_filename.replace(".uvt", "")
-    file_30m = get_30m_file(source_out, line_name[index][0], QN[index][0])
+    print(uvt_dir, Lid_i, file_uvt)
+    # uvt_file = uvt_dir + Lid_i + "/" + uvt_filename
+    # merge_30m = get_uvt_file(
+    #     source_out, line_name[index][0], QN[index][0], Lid_i, merge=True).replace(".uvt", "")
+    file_30m = get_30m_file(source_out, line_name_i,
+                            QN_name_i, Lid_i, merge=False)
+    merge_30m = get_30m_file(source_out, line_name_i,
+                             QN_name_i, Lid_i, merge=True)
 
-    os.system(f"rm {outputfile}.*")
+    os.system(f"rm {merge_30m[:-4]}.*")
     fb = tempfile.NamedTemporaryFile(
         delete=True, mode="w+", dir=".", suffix=".class")
     fb.write(f"file in {file_30m}\n")
-    fb.write(f"file out {outputfile}.30m  single /overwrite\n")
+    fb.write(f"file out {merge_30m}  single /overwrite\n")
     fb.write(f"say [INFO] Removing old output file\n")
-    fb.write(f'say "[INFO] Making new output file: {outputfile}"\n')
+    fb.write(f'say "[INFO] Making new output file: {merge_30m}"\n')
     fb.write(f"find\n")
     fb.write(f"set mode x auto\n")
     fb.write(f"set unit v f\n")
@@ -196,95 +254,114 @@ def line_prepare_merge(source_name: str, line_i: str, QN_i: str) -> None:
     fb.write(f"  write\n")
     fb.write(f"next\n")
     fb.write(f"sic message class s+i\n")
-    fb.write(f"file in {outputfile}.30m\n")
+    fb.write(f"file in {merge_30m}\n")
     fb.write(f"find /all\n")
-    fb.write(f"table {outputfile} new /NOCHECK source /like {uvt_file}\n")
+    fb.write(f"table {merge_uvt[:-4]} new /NOCHECK source /like {file_uvt}\n")
     # fb.write(f'xy_map {outputfile}\n')
     fb.write(f"exit\n")
     fb.flush()
     os.system("class -nw @ %s" % fb.name)
     fb.close()
     # copy to folder for merging
-    merged_folder = f"{uvt_dir_out}{Lid_i}"
+    # merged_folder = f"{uvt_dir_out}{Lid_i}"
+    merged_folder = os.path.dirname(merge_uvt)  # get path only
+
     if not os.path.exists(merged_folder):
         os.makedirs(merged_folder)
-    os.system("cp {0}.tab {1}/.".format(outputfile, merged_folder))
-    os.system("cp {0} {1}/.".format(uvt_file, merged_folder))
+    # os.system("cp {0}.tab {1}/.".format(outputfile, merged_folder))
+    os.system(f'cp {file_uvt} {merged_folder}/.')
 
 
-def line_reduce_30m(source_name: str, lines: str, QNs: str) -> None:
-    source, source_find, source_out, ra0, dec0, vlsr = get_source_param(
-        source_name)
+def line_reduce_30m(source_name: str, line_i: str, QN_i: str) -> None:
+    """
+    Function to perform a simple data reduction ot the 30m data.
+    Output spectra will be stores in Ta* scale.
+    It will ensure that the 30m data use the correct frequency and coordinate center.
+
+    parameters:
+    -----------
+    source_name: str
+        Name of the source to reduce, e.g., "B5"
+    line_i: str
+        Molecule to reduce, e.g., "CO", "13CO", "N2H+"
+    QN_i: str
+        Quantum numbers of the line to reduce, e.g., "1-0" or "N=1-0,J=3/2-1/2,F=1/2-1/2"
+    """
+    _, source_find, source_out, ra0, dec0, vlsr = get_source_param(source_name)
 
     freq_corr = 1 - vlsr / 3e5
     # get data files list
-    inputfiles = glob(f"{inputdir}/*.30m")
+    inputfiles = glob("*.30m", root_dir=inputdir)
     if len(inputfiles) == 0:
         raise ValueError(f"No files found in the input directory: {inputdir}")
 
-    for line_i, QN_i in zip(lines, QNs):
-        print(f"[INFO] Reducing line: {line_i} with QN: {QN_i}")
-        index = get_line_param(line_i, QN_i)
-        print(source_out, line_name[index][0], QN[index][0])
-        # Get frequency
-        freq_i = freq[index][0].astype(float) * 1e3
-        dv_base = vel_width_base_30m[index][0].astype(float)
-        dv = vel_width_30m[index[0]].astype(float)
-        vel_win = "{0:.2f}  {1:.2f}".format(vlsr - dv_base, vlsr + dv_base)
-        vel_ext = "{0:.2f}  {1:.2f}".format(vlsr - dv, vlsr + dv)
-        # Define output
-        outputfile = get_30m_file(
-            source_out, line_name[index][0], QN[index][0])
-        os.system("rm %s.*" % outputfile)
-        fb = tempfile.NamedTemporaryFile(
-            delete=True, mode="w+", dir=".", suffix=".class"
-        )
-        fb.write(f"file out {outputfile}  single\n")
-        fb.write(f"say [INFO] Removing old output file\n")
-        fb.write(f'say "[INFO] Making new output file: {outputfile}"\n')
-        ####
-        # Loop through files - one file per date
-        for inputfile in inputfiles:
-            for ignorefile in ignorefiles:
-                if ignorefile in inputfile:
-                    fb.write(f'say "[INFO] Ignoring file: {inputfile}"\n')
-                    continue
-            fb.write(f'say "[INFO] Processing file: {inputfile}"\n')
-            # Load file and det defaults
-            fb.write(f'file in "{inputfile}"\n')
-            fb.write(f"set source {source_find}\n")
-            fb.write(f"set tele *\n")
-            fb.write(f"set line *\n")
-            # Open and check file
-            # only observations with reference frequency
-            fb.write("find /frequency {0}\n".format(freq_i * freq_corr))
-            fb.write(f"set mode x auto\n")
-            fb.write(f"set unit v\n")
-            fb.write(f"get zero\n")
-            fb.write(f"sic message class s-i\n")
-            fb.write(f"for i 1 to found\n")
-            fb.write(f"  get next\n")
-            fb.write(f"  modify linename {name_str[index][0]}\n")
-            fb.write(f"  modify freq {freq_i}\n")
-            fb.write(f"  modify source {source_out}\n")
-            fb.write(f"  modify projection = {ra0} {dec0} =\n")
-            fb.write(f"  modify telescope 30M-MRT\n")
-            fb.write(f"  extract {vel_ext} velocity\n")  # cut out spectra
-            fb.write(f"  set window {vel_win}\n")  # define baseline window
-            fb.write(f"  base 1\n")  # first order baseline
-            fb.write(f"  write\n")
-            fb.write(f"next\n")
-            # ! Toggle back screen informational messages
-            fb.write(f"sic message class s+i\n")
-        # Now process the whole dataset available
-        # Regrid and output to fits file
-        print(outputfile)
-        fb.write(f"file in {outputfile}\n")
-        fb.write(f"find /all\n")
-        fb.write(f"table {outputfile[:-4]} new /nocheck\n")
-        fb.write(f"xy_map {outputfile[:-4]}\n")
-        fb.write(f"exit\n")
-        fb.flush()
-        os.system(f"rm -f {outputfile}")
-        os.system(f"class -nw @ {fb.name}")
-        fb.close()
+    print(f"[INFO] Reducing line: {line_i} with QN: {QN_i}")
+    index = get_line_param(line_i, QN_i)
+    print(source_out, line_name[index][0], QN[index][0])
+    # Get frequency
+    Lid_i = Lid[index][0]
+    QN_name_i = QN[index][0]
+    line_name_i = line_name[index][0]
+    freq_i = freq[index][0].astype(float) * 1e3
+    dv_base = vel_width_base_30m[index][0].astype(float)
+    dv = vel_width_30m[index[0]].astype(float)
+    vel_win = "{0:.2f}  {1:.2f}".format(vlsr - dv_base, vlsr + dv_base)
+    vel_ext = "{0:.2f}  {1:.2f}".format(vlsr - dv, vlsr + dv)
+    # Define output
+    file_30m = get_30m_file(source_out, line_name_i,
+                            QN_name_i, Lid_i, merge=False)
+    # outputfile = get_30m_file(
+    #     source_out, line_name[index][0], QN[index][0])
+    os.system(f"rm {file_30m[:-4]}.*")
+    fb = tempfile.NamedTemporaryFile(
+        delete=True, mode="w+", dir=".", suffix=".class")
+    fb.write(f"file out {file_30m}  single\n")
+    fb.write(f"say [INFO] Removing old output file\n")
+    fb.write(f'say "[INFO] Making new output file: {file_30m}"\n')
+    ####
+    # Loop through files - one file per date
+    for inputfile in inputfiles:
+        # check everyfile to be ignored if it is the current file
+        for ignorefile in ignorefiles:
+            if ignorefile in inputfile:
+                fb.write(f'say "[INFO] Ignoring file: {inputfile}"\n')
+                continue
+        fb.write(f'say "[INFO] Processing file: {inputfile}"\n')
+        # Load file and det defaults
+        fb.write(f'file in "{inputfile}"\n')
+        fb.write(f"set source {source_find}\n")
+        fb.write(f"set tele *\n")
+        fb.write(f"set line *\n")
+        # Open and check file
+        # only observations with reference frequency
+        fb.write("find /frequency {0}\n".format(freq_i * freq_corr))
+        fb.write(f"set mode x auto\n")
+        fb.write(f"set unit v\n")
+        fb.write(f"get zero\n")
+        fb.write(f"sic message class s-i\n")
+        fb.write(f"for i 1 to found\n")
+        fb.write(f"  get next\n")
+        fb.write(f"  modify linename {name_str[index][0]}\n")
+        fb.write(f"  modify freq {freq_i}\n")
+        fb.write(f"  modify source {source_out}\n")
+        fb.write(f"  modify projection = {ra0} {dec0} =\n")
+        fb.write(f"  modify telescope 30M-MRT\n")
+        fb.write(f"  extract {vel_ext} velocity\n")  # cut out spectra
+        fb.write(f"  set window {vel_win}\n")  # define baseline window
+        fb.write(f"  base 1\n")  # first order baseline
+        fb.write(f"  write\n")
+        fb.write(f"next\n")
+        # ! Toggle back screen informational messages
+        fb.write(f"sic message class s+i\n")
+    # Now process the whole dataset available
+    # Regrid and output to fits file
+    print(file_30m)
+    fb.write(f"file in {file_30m}\n")
+    fb.write(f"find /all\n")
+    fb.write(f"table {file_30m[:-4]} new /nocheck\n")
+    fb.write(f"xy_map {file_30m[:-4]}\n")
+    fb.write(f"exit\n")
+    fb.flush()
+    os.system(f"rm -f {file_30m}")
+    os.system(f"class -nw @ {fb.name}")
+    fb.close()
